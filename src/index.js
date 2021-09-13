c657f545ac361b8d733ead7f5a478c7f34488b73
AlmondRumble11 <54431988+AlmondRumble11@users.noreply.github.com>
2021-09-13 20:29:45 +0300

    fixed div element

--- src/index.js
+++ src/index.js
@@ -25,12 +25,20 @@ function ButtonFunctions() {
     let newListItem = document.createElement("li");
     let newDiv = document.createElement("div");
 
-    //first div elment = rating
-    const divRating = document.createTextNode(rating.value);
-    newDiv.appendChild(divRating);
-    //second div element = text
-    const divText = document.createTextNode(commentText.value);
-    newDiv.appendChild(divText);
+    //first div elment = rating and second div element = text
+    const ratingArticle = document.createElement("article");
+    const textArticle = document.createElement("article");
+
+    ratingArticle.innerHTML = rating.value;
+    textArticle.innerHTML = commentText.value;
+
+    ratingArticle.classList.add("comment-rating");
+    textArticle.classList.add("comment-text");
+
+    //divRating.classList.add("comment-rating");
+    //divText.classList.add("comment-text");
+    newDiv.appendChild(ratingArticle);
+    newDiv.appendChild(textArticle);
 
     //adding comment to new list item value
     //newListItem.innerHTML = commentText.value;
@@ -39,10 +47,9 @@ function ButtonFunctions() {
     //create a remove button
     const removeButton = document.createElement("button");
     removeButton.innerText = "Remove";
-    //removeButton.style.visibility = "hidden";
     removeButton.classList.add("remove-comment");
-
     newDiv.appendChild(removeButton);
+
     newListItem.appendChild(newDiv);
 
     //clearing the comment area
@@ -50,30 +57,30 @@ function ButtonFunctions() {
 
     //adding new list item to the comment list
     commentList.appendChild(newListItem);
+
+    removeButton.addEventListener("click", function () {
+      var action = window.confirm("Really want to remove all comments");
+      if (action === true) {
+        //https://stackoverflow.com/questions/36035736/add-remove-li-element-from-the-ul-javascript
+        newListItem.remove();
+      }
+    });
   });
 
   //getting the remove-comments button
   const removeCommentsButton = document.getElementById("remove-comments");
 
   //remove comments button event listener
-  /* removeCommentsButton.addEventListener("click", function () {
+  removeCommentsButton.addEventListener("click", function () {
     //getting comment list
 
     const removeBtn = document.getElementsByClassName("remove-comment");
     //number of comments
-    const element = document.getElementsByTagName("LI");
+    //const commentListLenght = commentList.getElementsByTagName("LI").length;
 
     //adding a delete button to all of the reviews
     for (var i = 0; i < removeBtn.length; i++) {
       removeBtn[i].style.visibility = "visible";
-      removeBtn[i].addEventListener("click", function () {
-        var action = true; //window.confirm("Really want to remove all comments");
-        if (action === true) {
-          //https://stackoverflow.com/questions/36035736/add-remove-li-element-from-the-ul-javascript
-          this[i].removeChild(this);
-        }
-      });
     }
-    //ask confirmation
-  });*/
+  });
 }
